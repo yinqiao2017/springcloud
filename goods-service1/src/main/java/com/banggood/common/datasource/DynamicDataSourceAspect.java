067eb3aa1e5ff11ac31fd26807deaa2aedde58b9
尹乔 <yinqiao@banggood.cn>
2017-03-31 10:11:26 +0800

    添加多数据源

---
+++ goods-service1/src/main/java/com/banggood/common/datasource/DynamicDataSourceAspect.java
@@ -0,0 +1,43 @@
+package com.banggood.common.datasource;
+
+/**
+ * Created by Administrator on 2017/3/30.
+ */
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.After;
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
+
+
+@Aspect
+@Order(-1)// 保证该AOP在@Transactional之前执行
+@Component
+public class DynamicDataSourceAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(DynamicDataSourceAspect.class);
+
+    @Before("@annotation(ds)")
+    public void changeDataSource(JoinPoint point, TargetDataSource ds) throws Throwable {
+        String dsId = ds.name();
+        if (!DynamicDataSourceContextHolder.containsDataSource(dsId)) {
+            logger.error("数据源[{}]不存在，使用默认数据源 > {}", ds.name(), point.getSignature());
+        } else {
+            logger.debug("Use DataSource : {} > {}", ds.name(), point.getSignature());
+            DynamicDataSourceContextHolder.setDataSourceType(ds.name());
+        }
+    }
+
+    @After("@annotation(ds)")
+    public void restoreDataSource(JoinPoint point, TargetDataSource ds) {
+        logger.debug("Revert DataSource : {} > {}", ds.name(), point.getSignature());
+        DynamicDataSourceContextHolder.clearDataSourceType();
+    }
+
+}
