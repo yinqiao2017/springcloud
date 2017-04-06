ba6bf6b88329bf9581dfac50aae6493063e5d9e6
尹乔 <yinqiao@banggood.cn>
2017-04-06 09:18:06 +0800

    1

--- goods-service1/src/main/java/com/banggood/controller/GoodsController.java
+++ goods-service1/src/main/java/com/banggood/controller/GoodsController.java
@@ -3,6 +3,8 @@ package com.banggood.controller;
 import com.alibaba.fastjson.JSON;
 import com.banggood.entity.Goods;
 import com.banggood.service.GoodsService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
+    @ApiOperation(value="获取商品列表", notes="")
     @GetMapping
     public List<Goods> getGoods() {
         List<Goods> goodsList = goodsService.selectAll();
@@ -28,6 +31,8 @@ public class GoodsController {
         return goodsList;
     }
 
+    @ApiOperation(value="获取商品详细信息", notes="根据url的id来获取商品详细信息")
+    @ApiImplicitParam(name = "goods_id", value = "商品id", required = true, dataType = "Long")
     @GetMapping("/{goods_id}")
     public Goods getGoodsById(@PathVariable("goods_id") Long goods_id) {
         Goods goods = goodsService.selectByKey(goods_id);
@@ -35,6 +40,8 @@ public class GoodsController {
         return goods;
     }
 
+    @ApiOperation(value="添加商品", notes="根据商品对象添加商品")
+    @ApiImplicitParam(name = "goods", value = "商品实体goods", required = true, dataType = "goods")
     @PostMapping
     public void add(@RequestBody Goods goods) {
 
@@ -42,16 +49,20 @@ public class GoodsController {
         goodsService.insert(goods);
     }
 
-    @DeleteMapping("/{goods_id}")
-    public void delGoods(@PathVariable("goods_id") Long goods_id) {
-        log.info("根据ID删除商品："+JSON.toJSONString(goodsService.selectByKey(goods_id)));
-        goodsService.delete(goods_id);
-    }
-
+    @ApiOperation(value = "修改商品", notes = "根据id修改对应商品的详细信息")
+    @ApiImplicitParam(name = "goods", value = "商品实体goods", required = true, dataType = "goods")
     @PutMapping("/{goods_id}")
     public void updateGoods(@RequestBody Goods goods) {
 
         log.info("修改商品："+JSON.toJSONString(goods));
         goodsService.updateByPrimaryKey(goods);
+    }
+
+    @ApiOperation(value = "删除商品", notes = "根据商品ID删除商品")
+    @ApiImplicitParam(name = "goods_id", value = "商品ID", required = true, dataType = "Long")
+    @DeleteMapping("/{goods_id}")
+    public void delGoods(@PathVariable("goods_id") Long goods_id) {
+        log.info("根据ID删除商品："+JSON.toJSONString(goodsService.selectByKey(goods_id)));
+        goodsService.delete(goods_id);
     }
 }
